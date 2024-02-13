22c13e81bf83862a7415bdbe08894cd5d4fe6400
tatiuska <tfweska@gmail.com>
2024-02-13 16:33:25 -0300

    exercício operadores

---
+++ operadores/operadores.js
@@ -0,0 +1,15 @@
+// Operadores
+var num = 0;
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++;
+num--;
+num += 1;
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+
+console.log('num == 1 : ' + (num == 1));
+

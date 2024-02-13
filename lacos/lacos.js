aea13481b7462568dad00ca68054c1f916eabbf9
tatiuska <tfweska@gmail.com>
2024-02-13 19:28:03 -0300

    exercícios laços - for, while, do/while.

---
+++ lacos/lacos.js
@@ -0,0 +1,19 @@
+// Laços
+// For
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// While
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+// Do/While
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 10);

456f323260a09255de6e3da6c3b18358807dcf75
tatiuska <tfweska@gmail.com>
2024-02-14 10:09:42 -0300

    poo - objetos

---
+++ poo/objetos.js
@@ -0,0 +1,14 @@
+// Programação Orientada a Objetos (POO)
+// Criando um objeto simples em JS
+var obj = new Object(); // uma maneira
+
+var obj = {}; // outra maneira
+
+// criando um objeto completo
+obj = {
+    name: {
+        first: 'Gandalf',
+        last: 'the Grey'
+    },
+    address: 'Middle Earth'
+};
